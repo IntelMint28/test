f944fda0837c119ec50cf674d2f24df6a4723585
IntelMint28 <discord.intel28@gmail.com>
2025-11-11 20:34:43 -0600

    owo

--- TetoSilentCloner.lua
+++ TetoSilentCloner.lua
@@ -137,7 +137,7 @@ local function silentClone()
     
     local clone = hrp:Clone()
     clone.Parent = char
-    clone.CFrame = hrp.CFrame + hrp.CFrame.LookVector * 2
+    clone.CFrame = hrp.CFrame + hrp.CFrame.LookVector * 3
     hrp.CFrame = clone.CFrame
     clone:Destroy()
     
